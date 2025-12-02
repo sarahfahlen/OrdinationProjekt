ed0e75e978f554e5942b5ae9a995295a23270d16
FrederikSA <eaa24fsan@students.eaaa.dk>
2025-12-02 14:37:32 +0100

    Daglig skæv test færdig

--- ordination-test/DagligSkævTest.cs
+++ ordination-test/DagligSkævTest.cs
@@ -9,5 +9,151 @@ using shared.Model;
 [TestClass]
 public class DagligSkævTest
 {
+    private DataService service;
+    private Laegemiddel lm = null!;
+
+    [TestInitialize]
+    public void SetupBeforeEachTest()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<OrdinationContext>();
+        optionsBuilder.UseInMemoryDatabase(databaseName: "test-database");
+        var context = new OrdinationContext(optionsBuilder.Options);
+        service = new DataService(context);
+        service.SeedData();
+        lm = service.GetLaegemidler().First();
+    }
     
+    [TestMethod]
+    public void Antalover0()
+    {
+        Assert.IsTrue(service.GetDagligSkæve().Count() > 0);
+    }
+
+    [TestMethod]
+    public void Antalunder0()
+    {
+        var skæv = service.GetDagligSkæve().First();
+
+        try
+        {
+            skæv.opretDosis(DateTime.Now, -1);
+            Assert.Fail("Forventede at opretDosis kastede en exception");
+        }
+        catch (ArgumentException ex)
+        {
+            Assert.AreEqual("Antal må ikke være negativt.", ex.Message);
+        }
+    }
+
+    [TestMethod]
+    public void OpretDosis_NegativAntal_KasterException()
+    {
+        var skæv = service.GetDagligSkæve().First();
+
+        try
+        {
+            skæv.opretDosis(DateTime.Now, -5);
+            Assert.Fail("Forventede at opretDosis kastede en exception");
+        }
+        catch (ArgumentException ex)
+        {
+            Assert.AreEqual("Antal må ikke være negativt.", ex.Message);
+        }
+    }
+    
+    [TestMethod]
+    public void DagligSkaev_StartEfterSlut()
+    {
+        var laegemiddel = service.GetLaegemidler().First();
+
+        try
+        {
+            var skæv = new DagligSkæv(
+                new DateTime(2025, 2, 10), // start
+                new DateTime(2025, 2, 1),  // slut
+                laegemiddel
+            );
+            Assert.Fail("Forventede at konstruktøren kastede en exception");
+        }
+        catch (ArgumentException ex)
+        {
+            Assert.AreEqual("Startdato må ikke være efter slutdato.", ex.Message);
+        }
+    }
+
+
+    [TestMethod]
+    public void Skæv_SamletDosis_Korrekt()
+    {
+        var skæv = new DagligSkæv(
+            new DateTime(2025, 1, 1),
+            new DateTime(2025, 1, 3),
+            lm
+        );
+
+        skæv.doser.Clear();
+
+        skæv.opretDosis(DateTime.Now, 2);
+        skæv.opretDosis(DateTime.Now, 3);
+        
+        double resultat = skæv.samletDosis();
+        
+        Assert.AreEqual(15, resultat);
+    }
+
+    [TestMethod]
+    public void DoegnDosis_UdenDoser_Giver0()
+    {
+        var skæv = new DagligSkæv(DateTime.Now, DateTime.Now, lm);
+
+        skæv.doser.Clear(); // sikrer ingen doser
+        
+        double resultat = skæv.doegnDosis();
+        
+        Assert.AreEqual(0, resultat);
+    }
+
+    [TestMethod]
+    public void DoegnDosis_MedEnDosis_Korrekt()
+    {
+        var skæv = new DagligSkæv(DateTime.Now, DateTime.Now, lm);
+
+        skæv.doser.Clear();
+        skæv.opretDosis(DateTime.Now, 4.5);
+        
+        double resultat = skæv.doegnDosis();
+        
+        Assert.AreEqual(4.5, resultat);
+    }
+    
+    [TestMethod]
+    public void DoegnDosis_MedFlereDoser_Korrekt()
+    {
+        var skæv = new DagligSkæv(DateTime.Now, DateTime.Now, lm);
+
+        skæv.doser.Clear();
+        skæv.opretDosis(DateTime.Now, 1);
+        skæv.opretDosis(DateTime.Now, 2.5);
+        skæv.opretDosis(DateTime.Now, 3);
+        // doegnDosis = 1 + 2.5 + 3 = 6.5
+        
+        double resultat = skæv.doegnDosis();
+        
+        Assert.AreEqual(6.5, resultat);
+    }
+
+    [TestMethod]
+    public void GetType_Skæv()
+    {
+        var skæv = new DagligSkæv(
+            DateTime.Now,
+            DateTime.Now,
+            lm
+        );
+        
+        string type = skæv.getType();
+        
+        Assert.AreEqual("DagligSkæv", type);
+    }
+
 }
--- shared/Model/DagligSkæv.cs
+++ shared/Model/DagligSkæv.cs
@@ -3,18 +3,31 @@ namespace shared.Model;
 public class DagligSkæv : Ordination {
     public List<Dosis> doser { get; set; } = new List<Dosis>();
 
-    public DagligSkæv(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel) : base(laegemiddel, startDen, slutDen) {
-	}
+    public DagligSkæv(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel)
+	    : base(laegemiddel, startDen, slutDen)
+    {
+	    if (startDen > slutDen)
+		    throw new ArgumentException("Startdato må ikke være efter slutdato.");
+    }
 
-    public DagligSkæv(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel, Dosis[] doser) : base(laegemiddel, startDen, slutDen) {
-        this.doser = doser.ToList();
-    }    
+    public DagligSkæv(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel, Dosis[] doser)
+	    : base(laegemiddel, startDen, slutDen)
+    {
+	    if (startDen > slutDen)
+		    throw new ArgumentException("Startdato må ikke være efter slutdato.");
+
+	    this.doser = doser.ToList();
+    }
 
     public DagligSkæv() : base(null!, new DateTime(), new DateTime()) {
     }
 
-	public void opretDosis(DateTime tid, double antal) {
-        doser.Add(new Dosis(tid, antal));
+    public void opretDosis(DateTime tid, double antal)
+    {
+	    if (antal < 0)
+		    throw new ArgumentException("Antal må ikke være negativt.");
+
+	    doser.Add(new Dosis(tid, antal));
     }
 
 	public override double samletDosis() {
